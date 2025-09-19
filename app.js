c8a97e6a25cc6a4593ce3cb13e308cafe07a81f4
emersondemetrio <emerson@ae.studio>
2025-09-19 08:54:40 -0300

    fix port railway

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ fastify.get('/', async function handler (request, reply) {
 })
 
 try {
-  await fastify.listen({ port })
+  await fastify.listen({ port, host: '0.0.0.0' })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
