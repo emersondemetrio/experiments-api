4135f5f683d6bb5a4ec7abe8a8babe8254cfa80a
emersondemetrio <emerson@ae.studio>
2025-09-19 13:10:05 -0300

    [0] fix file name

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use('/', routes)
 
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`)
+})
+
+server.on('error', (err) => {
+  console.error('Server error:', err)
+  process.exit(1)
+})
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err)
+  process.exit(1)
 })
