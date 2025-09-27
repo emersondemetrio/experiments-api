53fbb6dbb0a0c826b1d08a624aa38aefb433d53a
emersondemetrio <emerson@ae.studio>
2025-09-27 17:07:43 -0400

    test

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.json({ hello: 'world' })
+  res.json({ hello: 'world', nowIt: "goes" })
 })
 
 export default router
